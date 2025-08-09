0e75f966ac693c0239cfc37d94a2b59277187efc
kaneki <kaneki@gmail.com>
2025-08-10 00:54:03 +0800

    使用env初始化参数传入agent

---
+++ vars/colors.groovy
@@ -0,0 +1,46 @@
+// utils/var.groovy
+
+de fblack   = "\u001B[30m"
+def red     = "\u001B[31m"
+def green   = "\u001B[32m"
+def yellow  = "\u001B[33m"
+def blue    = "\u001B[34m"
+def purple  = "\u001B[35m"
+def cyan    = "\u001B[36m"
+def white   = "\u001B[37m"
+
+// 亮色字体
+def brightBlack   = "\u001B[90m"
+def brightRed     = "\u001B[91m"
+def brightGreen   = "\u001B[92m"
+def brightYellow  = "\u001B[93m"
+def brightBlue    = "\u001B[94m"
+def brightPurple  = "\u001B[95m"
+def brightCyan    = "\u001B[96m"
+def brightWhite   = "\u001B[97m"
+
+// 背景色（可用于强调整行）
+def bgBlack   = "\u001B[40m"
+def bgRed     = "\u001B[41m"
+def bgGreen   = "\u001B[42m"
+def bgYellow  = "\u001B[43m"
+def bgBlue    = "\u001B[44m"
+def bgPurple  = "\u001B[45m"
+def bgCyan    = "\u001B[46m"
+def bgWhite   = "\u001B[47m"
+
+// 亮色背景
+def bgBrightBlack   = "\u001B[100m"
+def bgBrightRed     = "\u001B[101m"
+def bgBrightGreen   = "\u001B[102m"
+def bgBrightYellow  = "\u001B[103m"
+def bgBrightBlue    = "\u001B[104m"
+def bgBrightPurple  = "\u001B[105m"
+def bgBrightCyan    = "\u001B[106m"
+def bgBrightWhite   = "\u001B[107m"
+
+// 样式
+def bold      = "\u001B[1m"
+def underline = "\u001B[4m"
+def reversed  = "\u001B[7m"
+def reset     = "\u001B[0m"
